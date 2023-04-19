8b696eaf7f987da0a36895c1f3c23f567fd0921a
AlexGrigorenco <alpsnhandy@gmail.com>
2023-04-19 16:01:08 +0300

    init

---
+++ main.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){  
+
+    $('.bxslider').bxSlider({
+        pager: false,
+        infiniteLoop: false,
+        controls: true,
+        maxSlides: 4,
+        minSlides: 1,
+        slideMargin: 38,
+        slideWidth: 'auto',
+        nextText: `<span></span>`,
+		    prevText: `<span></span>`,
+        touchEnabled: false,
+    }); 
+
+    Fancybox.bind('[data-fancybox="gallery"]', {
+      //
+    });
+  });
+
+
+  
